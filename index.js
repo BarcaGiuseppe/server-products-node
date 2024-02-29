520c7b464f94992858d274e9acc5acca456fd9d7
BarcaGiuseppe <gbarca1995@gmail.com>
2024-02-29 14:09:04 +0100

    fix: fix port and package

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const port = process.env.port || 3000;
 const productsRoute = require("./routes/products");
 app.use(cors());
 //app.use(express.json());
 app.use("/products", productsRoute);
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
