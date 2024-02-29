44f337d073508539af91718e343ae24d3c4fbaba
BarcaGiuseppe <gbarca1995@gmail.com>
2024-02-29 14:14:05 +0100

    fix: fix get method

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const cors = require("cors");
 const port = process.env.port || 3000;
 const productsRoute = require("./routes/products");
 app.use(cors());
+
 //app.use(express.json());
-app.use("/products", productsRoute);
+//app.use("/products", productsRoute);
+app.get("/", async (req, res) => {
+  const response = await fetch("https://mockend.up.railway.app/api/products");
+  const products = await response.json();
+  res.send(products);
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
